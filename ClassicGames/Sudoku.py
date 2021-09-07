0764bc69a968c79b2940c4a6192edc498f562347
yang-yc-squirrel <2109270989@qq.com>
2021-09-07 20:06:16 +0800

    sudoko

---
+++ ClassicGames/Sudoku.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot
+import numpy
+
+p=numpy.arange(0,5,1)
+for i in p:
+    x1=numpy.array([i]*100)
+    y1=numpy.linspace(0,4,100)
+    matplotlib.pyplot.plot(x1,y1,color=(0,0,0))
+    x2=numpy.linspace(0,4,100)
+    y2=numpy.array([i]*100)
+    matplotlib.pyplot.plot(x2,y2,color=(0,0,0))
+
+
+matplotlib.pyplot.show()
