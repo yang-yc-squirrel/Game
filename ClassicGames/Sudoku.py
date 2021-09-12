a4a9e02caf3f155c93faf67155ba481e069067f1
yang-yc-squirrel <2109270989@qq.com>
2021-09-12 11:43:48 +0800

    test

--- ClassicGames/Sudoku.py
+++ ClassicGames/Sudoku.py
@@ -19,5 +19,9 @@ for i in p:
     y2 = numpy.array([i] * 100)
     matplotlib.pyplot.plot(x2, y2, color=(0, 0, 0), linewidth=1.5)
 
+exist_parts=[[1,1,2],[2,1,3],[5,5,2]]
+for number in exist_parts:
+    words=str(number[2])
+
 matplotlib.pyplot.axis("off")
 matplotlib.pyplot.show()
