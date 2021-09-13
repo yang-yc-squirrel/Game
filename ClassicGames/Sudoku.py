3f6b75bb8f335d941a8698e5b70eda9e50f11c27
yang-yc-squirrel <2109270989@qq.com>
2021-09-13 18:39:44 +0800

    test

--- ClassicGames/Sudoku.py
+++ ClassicGames/Sudoku.py
@@ -19,6 +19,13 @@ for i in p:
     y2 = numpy.array([i] * 100)
     matplotlib.pyplot.plot(x2, y2, color=(0, 0, 0), linewidth=1.5)
 
+basic_axis=[]
+for i in range(9,0,-3):
+    for j in range(0,9,3):
+        basic_axis.append([j,i])
+
+add_axis=[]
+
 exist_parts=[[1,1,2],[2,1,3],[5,5,2]]
 for number in exist_parts:
     words=str(number[2])
